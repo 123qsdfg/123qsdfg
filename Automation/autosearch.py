09406a9cd4b81ed7e05507b92d5aaea6c4cbe53e
andy.sc <131955574+123qsdfg@users.noreply.github.com>
2024-07-21 14:54:38 +0900

    first commit

---
+++ Automation/autosearch.py
@@ -0,0 +1,21 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    comic = input("검색어를 입력하세요")
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://page.kakao.com/")
+    
+    page.get_by_placeholder("제목, 작가를 입력하세요").click()
+    for i in range(50):
+        page.get_by_placeholder("제목, 작가를 입력하세요").fill(comic)
+        page.get_by_placeholder("제목, 작가를 입력하세요").press("Enter")
+    # ---------------------
+    context.close()
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
